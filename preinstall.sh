a004f0e333ce0adedf0902a693803ef88aadc5ab
Jan <mig@jankjr.dk>
2025-03-17 11:05:20 +0100

    preinstall script

---
+++ preinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# If linux check if cargo is installed
+if [ "$(uname)" == "Linux" ]; then
+  if ! command -v cargo &>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  fi
+
+  # Install dependencies
+  apt-get install build-essential
+  apt-get install pkg-config
+  apt-get install libudev-dev
+fi
